545e7ec3de71f45c05d4922e113b0d86f60bef55
Akhil Goel <Akhil Goel>
2017-04-01 12:56:12 +0530

    socket try

--- hello.py
+++ hello.py
@@ -25,7 +25,7 @@ def test():
     BUFFER_SIZE = 1024
     MESSAGE="Hello World"
 
-    s = socket.socket(socket.AF_INET,socket.SCOK_STREAM)
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect((TCP_IP,TCP_PORT))
     s.send(MESSAGE)
     data = s.recv(BUFFER_SIZE)
